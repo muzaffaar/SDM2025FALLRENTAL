a7f07af8d1fdbcc1c8a43b638b8269d5f3f8053a
Muzaffar Tursunov <muzaffart949@gmail.com>
2025-10-24 15:45:59 +0200

    feat: Confirm all Student & Landlord core features work as expected

---
+++ database/factories/RentalFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rental>
+ */
+class RentalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomFloat(2, 100, 2000),
+            'location' => $this->faker->city(),
+            'landlord_id' => 0, // default 0
+            'status' => 'available',
+        ];
+    }
+}
